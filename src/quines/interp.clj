613dd9e6f8bb5046e51ae4e4af38a5c87308a22c
Oregu <oleg.burykin@gmail.com>
2013-07-27 21:55:47 +0400

    Switch vectors to lists

--- src/quines/interp.clj
+++ src/quines/interp.clj
@@ -20,19 +20,23 @@
       (lookup exp env)
     (and (= 'fn (first exp)) (not-in-env 'fn env))
       (let [x (second exp) body (first (next (next exp)))]
-        ['closure x body env])
+        (list 'closure x body env))
     :else
       (let [[rator rand] exp
         proc (eval-exp rator env)
         arg (eval-exp rand env)]
         (when (= 'closure (first proc))
-          (let [[cl x body env2] proc]
+          (let [[_ x body env2] proc]
             (eval-exp body (into [[x arg]] env2)))))))
 
+(def big-omega
+  '((fn fn [fn fn] [fn fn])
+    (fn fn [fn fn] [fn fn])))
+
+;; Evaluates to (fn z z)
 (defn test-1 []
   (eval-exp '(((fn x (fn y x)) (fn z z)) (fn a a)) '()))
 
+;; Will go to infinite loop, it's Big Omega time
 (defn test-2 []
-  (eval-exp
-    '[(fn fn [fn fn] [fn fn])
-      (fn fn [fn fn] [fn fn])] '()))
+  (eval-exp big-omega '()))
