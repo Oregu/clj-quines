f30eb4cbbcf3311581f9144d8686123f56cc66a1
Oregu <oleg.burykin@gmail.com>
2014-03-01 19:43:10 +0400

    core.logic version update

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject quines "0.3.0"
+(defproject quines "0.5.0"
   :description "Translating miniKanren quines relational interpreter to clojure core.logic."
   :url "http://github.com/Oregu/clj-quines"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.5"]])
+                 [org.clojure/core.logic "0.8.7"]]
+
+  :plugins [[lein-ancient "0.5.5-SNAPSHOT"]])
