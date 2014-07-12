d14878ace016eb2aa655e7f3debe9f70095d55f2
Oregu <oleg.burykin@gmail.com>
2014-07-12 18:14:24 +0400

    upgrade clojure and core.logic

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Translating miniKanren quines relational interpreter to clojure core.logic."
   :url "http://github.com/Oregu/clj-quines"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.7"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.8"]])
